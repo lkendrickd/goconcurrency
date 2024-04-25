8af9bd0c05c096670fbbc8db9d2b9327ce11e884
Dennis Kendrick <denniskendrick@gmail.com>
2024-04-25 16:21:47 -0600

    Update semaphore.go

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -7,44 +7,54 @@ import (
 	"time"
 )
 
-// Task represents a task to be processed
-// using a pointer to a bool to reflect changes outside of DoWork
+// Task is a struct that holds the data for a task
+// you could add other fields here like Data which you could use to pass
+// data to the task to be processed.
 type Task struct {
 	ID          int
-	IsProcessed *bool
+	IsProcessed bool
 }
 
 // WorkerWithSemaphore is a worker that processes tasks concurrently
-// the semaphore is used to limit the number of concurrent tasks
-// the maxConcurrent parameter specifies the maximum number of tasks
-// that can ever run concurrently.
+// the semaphore which is a buffered channel is used to control the number of concurrent
+// tasks that can be processed at a time.
 type WorkerWithSemaphore struct {
-	tasks         []*Task
+	tasks []*Task
+	// results is a channel that will hold the results of the work
+	// this could be a string, struct or any other type that you want to return
 	results       chan string
 	semaphore     chan struct{}
 	maxConcurrent int
 }
 
-// NewWorkerWithSemaphore creates a new concreate instance of WorkerWithSemaphore
-func NewWorkerWithSemaphore(tasks []*Task, maxConcurrent int) *WorkerWithSemaphore {
+// New creates a new WorkerWithSemaphore
+func New(tasks []*Task, maxConcurrent int) *WorkerWithSemaphore {
 	return &WorkerWithSemaphore{
-		tasks:   tasks,
+		tasks: tasks,
+		// buffer the channel with the number of tasks
 		results: make(chan string, len(tasks)),
-		// create a buffered channel to ratelimit on the maxConcurrent value
+		// buffer the semaphore with the number of concurrent tasks allowed to run
 		semaphore:     make(chan struct{}, maxConcurrent),
 		maxConcurrent: maxConcurrent,
 	}
 }
 
-// Work processes the tasks concurrently using anonymous goroutiness
+// Work processes the tasks concurrently using anonymous goroutines
 func (w *WorkerWithSemaphore) Work() {
 	var wg sync.WaitGroup
 
 	for _, task := range w.tasks {
-		// Acquire the semaphore
+		// acquire the semaphore this is achieved by writing to the channel
+		// this will block if the channel is full. If full the code will wait until
+		// a slot is available. I use an empty struct as the value because it
+		// doesn't take up any memory.
 		w.semaphore <- struct{}{}
+
+		// once we can send to the semaphore we can increment the waitgroup
 		wg.Add(1)
 
+		// start a goroutine to process the task this will run concurrently for each task
+		// that we are iterating over
 		go func(task *Task) {
 			defer wg.Done()
 			defer func() { <-w.semaphore }()
@@ -52,16 +62,19 @@ func (w *WorkerWithSemaphore) Work() {
 		}(task)
 	}
 
-	// Wait for all tasks to complete and close the results channel
-	go func() {
-		wg.Wait()
-		close(w.results)
-	}()
+	// after all the tasks are executed concurrently we wait for all the goroutines to finish
+	// this means we wait for all the wg.Done() calls to be executed which will decrement the counter
+	// to zero and this blocking call will proceed.
+	wg.Wait()
+
+	// close the results channel once all the tasks are processed
+	// even though the channel is closed we can still read from it
+	close(w.results)
 }
 
-// DoWork simulates work by sleeping for a random duration
+// DoWork simulates work on a task
 func (w *WorkerWithSemaphore) DoWork(task *Task) {
-	if *task.IsProcessed {
+	if task.IsProcessed {
 		return
 	}
 
@@ -70,13 +83,12 @@ func (w *WorkerWithSemaphore) DoWork(task *Task) {
 	for i := 0; i < int(workTime.Seconds()); i++ {
 		time.Sleep(1 * time.Second)
 	}
-
-	*task.IsProcessed = true
+	task.IsProcessed = true
 	fmt.Printf("Task %d is Completed\n", task.ID)
 	w.results <- fmt.Sprintf("Task %d completed in %v", task.ID, workTime)
 }
 
-// Results returns a channel to receive results from the worker
+// Results returns the results of the work
 func (w *WorkerWithSemaphore) Results() <-chan string {
 	return w.results
 }
